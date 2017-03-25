adad6b3f4bd03c0acf7443611f32a41ce7e8594c
bagdaterlanuly <bagdat.tashmanbet@ce.sdu.edu.kz>
2017-03-25 23:41:28 +0600

    comment

--- script.js
+++ script.js
@@ -2,8 +2,13 @@ angular.module('myApp', []).controller('todoCtrl', function($scope) {
     $scope.todoList = [];
 
     $scope.todoAdd = function() {
-        $scope.todoList.push({todoText:$scope.todoInput, done:true});
-        $scope.todoInput = "";
+        if ($scope.todoInput === "") {
+            $scope.ss=1;
+        }else{
+            $scope.todoList.push({todoText:$scope.todoInput, done:true});
+            $scope.todoInput = "";
+        }
+        
     };
 
     $scope.remove = function() {
